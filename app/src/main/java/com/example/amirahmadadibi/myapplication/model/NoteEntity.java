a0194f1e03c3e104ec51a140d1a34596ec23ca8d
AmirahmadAdibi <amirahmadadibi@gmail.com>
2018-10-13 22:37:02 +0330

    craete model data entitiy for notes

---
+++ app/src/main/java/com/example/amirahmadadibi/myapplication/model/NoteEntity.java
@@ -0,0 +1,58 @@
+package com.example.amirahmadadibi.myapplication.model;
+
+import java.util.Date;
+
+//it's going to be come a components that use Room library
+public class NoteEntity {
+    private int id;
+    private Date date;
+    private String text;
+    //create and assign values individualy
+    public NoteEntity() {
+
+    }
+    //edit
+    public NoteEntity(int id, Date date, String text) {
+        this.id = id;
+        this.date = date;
+        this.text = text;
+    }
+    //new with auto id
+    public NoteEntity(Date date, String text) {
+        this.date = date;
+        this.text = text;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+    //for  debugning
+    @Override
+    public String toString() {
+        return "NoteEntity{" +
+                "id=" + id +
+                ", date=" + date +
+                ", text='" + text + '\'' +
+                '}';
+    }
+}
